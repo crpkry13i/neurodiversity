bfba5ec8bc0449b8f12af47936a5a91c26b1887e
Rylei <ryleinathaniel@gmail.com>
2023-09-05 06:01:27 -0500

    Added random cat pics function. API, JS, & Axios.

---
+++ app.js
@@ -0,0 +1,40 @@
+// Random Cat Photo
+// fetch("https://api.thecatapi.com/v1/images/search?")
+// .then(res  => {
+//   console.log("RESOLVED!", res);
+//   return res.json()
+// })
+// .then((data) => {
+//   console.log(data);
+// })
+// .catch(e => {
+//   console.log("ERROR!", e);
+// })
+
+// const loadRandomCatPhoto = async () => {
+//   try {
+//   const res = await fetch("https://api.thecatapi.com/v1/images/search?");
+//   const data = await res.json();
+//   console.log(data[0].url);
+//   } catch(e) {
+//     console.log("ERROR!!!", e);
+//   }
+// }
+
+const catImg = document.querySelector('#catImg')
+const button = document.querySelector('#clickme')
+
+const loadRandomCatPhoto = async () => {
+  try {
+    const res = await axios.get("https://api.thecatapi.com/v1/images/search?");
+    return res.data[0].url
+  } catch(e) {
+    console.log("ERROR!!", e);
+  }
+}
+const addRandomCatPhoto = async () => {
+  const imgUrl = await loadRandomCatPhoto();
+  catImg.src = imgUrl
+}
+
+button.addEventListener('click', addRandomCatPhoto);
