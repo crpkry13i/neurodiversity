99b2536603f63373dd440e1367940e76175fa5a7
Rylei <ryleinathaniel@gmail.com>
2023-09-06 04:08:40 -0500

    Added some styles

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 // Random Cat Photo
+const catImg = document.querySelector('#catImg')
+const button = document.querySelector('#clickme')
+
+const loadRandomCatPhoto = async () => {
+  try {
+    const res = await axios.get("https://api.thecatapi.com/v1/images/search?");
+    return res.data[0].url
+  } catch(e) {
+    console.log("ERROR!!", e);
+  }
+}
+const addRandomCatPhoto = async () => {
+  const imgUrl = await loadRandomCatPhoto();
+  catImg.src = imgUrl
+}
+
+button.addEventListener('click', addRandomCatPhoto);
+
 // fetch("https://api.thecatapi.com/v1/images/search?")
 // .then(res  => {
 //   console.log("RESOLVED!", res);
@@ -20,21 +38,3 @@
 //     console.log("ERROR!!!", e);
 //   }
 // }
-
-const catImg = document.querySelector('#catImg')
-const button = document.querySelector('#clickme')
-
-const loadRandomCatPhoto = async () => {
-  try {
-    const res = await axios.get("https://api.thecatapi.com/v1/images/search?");
-    return res.data[0].url
-  } catch(e) {
-    console.log("ERROR!!", e);
-  }
-}
-const addRandomCatPhoto = async () => {
-  const imgUrl = await loadRandomCatPhoto();
-  catImg.src = imgUrl
-}
-
-button.addEventListener('click', addRandomCatPhoto);
