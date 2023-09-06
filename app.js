b5004aa424235820002f67aec0479bd650764b67
Rylei <ryleinathaniel@gmail.com>
2023-09-06 04:33:30 -0500

    Cleaned up code

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // Random Cat Photo
-const catImg = document.querySelector('#catImg')
-const button = document.querySelector('#clickme')
+const catImg = document.querySelector('#catImg');
+const button = document.querySelector('#clickme');
 
 const loadRandomCatPhoto = async () => {
   try {
     const res = await axios.get("https://api.thecatapi.com/v1/images/search?");
-    return res.data[0].url
+    return res.data[0].url;
   } catch(e) {
     console.log("ERROR!!", e);
   }
 }
 const addRandomCatPhoto = async () => {
   const imgUrl = await loadRandomCatPhoto();
-  catImg.src = imgUrl
+  catImg.src = imgUrl;
 }
 
 button.addEventListener('click', addRandomCatPhoto);
