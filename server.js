dcde9e96fe0789580e33f4b97669799fd3818d77
Rylei <ryleinathaniel@gmail.com>
2023-09-18 05:50:23 -0500

    Fixed fake contact form

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -11,12 +13,19 @@ app.get('/', (req, res) => {
   // res.send('Hello from Rylei\'s Sanctuary')
   res.render('index');
 })
+
+// Contact Form
 app.get('/contact', (req, res) => {
   res.render('contact');
 })
+app.post('/contact', (req, res) => {
+  res.render('contact');
+})
+
 app.get('/misc', (req, res) => {
   res.render('misc');
 })
+
 app.get('*', (req, res) => {
   res.send('You\'ve wondered off too far. Let\'s go back, shall we?')
 })
